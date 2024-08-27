6793e4987deca00d42e0756b8e2ebfaf1999042b
MeterCai <cai.mingjun@gmail.com>
2024-08-28 03:36:56 +0800

    Update SeamlessTile.py

--- SeamlessTile.py
+++ SeamlessTile.py
@@ -7,7 +7,8 @@ from torch import Tensor
 from torch.nn import Conv2d
 from torch.nn import functional as F
 from torch.nn.modules.utils import _pair
-
+import comfy.samplers
+import nodes
 
 class SeamlessTile:
     @classmethod
@@ -152,3 +153,35 @@ class OffsetImage:
         y = round(y * y_percent / 100)
         x = round(x * x_percent / 100)
         return (pixels.roll((y, x), (1, 2)),)
+
+class Tiled_KSampler:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required":
+                {"model": ("MODEL", ),
+                 "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                 "tiling": ("INT", {"default": 1, "min": 0, "max": 1}),
+                 "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+                 "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0}),
+                 "sampler_name": (comfy.samplers.KSampler.SAMPLERS, ),
+                 "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
+                 "positive": ("CONDITIONING", ),
+                 "negative": ("CONDITIONING", ),
+                 "latent_image": ("LATENT", ),
+                 "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
+                 }}
+
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "sample"
+
+    CATEGORY = "Sampling/Tiled"
+    def apply_circular(self, model, enable):
+        for layer in [layer for layer in model.modules() if isinstance(layer, torch.nn.Conv2d)]:
+            layer.padding_mode = 'circular' if enable else 'zeros'
+
+    def sample(self, model, seed, tiling, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=1.0):
+        self.apply_circular(model.model, tiling == 1)
+        return nodes.common_ksampler(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise)
+
+
+
